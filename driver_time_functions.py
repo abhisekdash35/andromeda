be360d247b26d5cd47f9c098b798b9de7bb89219
Abhisek Dash <abhisekdash35@gmail.com>
2020-12-15 12:25:40 +0530

    Delete driver_time_functions.py

--- driver_time_functions.py
+++
@@ -1,7 +0,0 @@
-from time_functions import local_solar_time,local_sidereal_time,eot_offset
-import datetime
-print(eot_offset(datetime.datetime.now()))
-print(local_solar_time(datetime.datetime.now(),82.5,76.11,consider_eot_boolean=True))
-print(local_sidereal_time(datetime.datetime.now(),82.5,76.11))
-
-
