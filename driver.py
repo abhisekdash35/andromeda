f47ee9e92f7783858044f94671f351641538c329
Abhisek Dash <abhisekdash35@gmail.com>
2020-12-15 10:30:53 +0530

    Delete driver.py

--- driver.py
+++
@@ -1,4 +0,0 @@
-from timefunctions import local_solar_time,local_sidereal_time
-import datetime
-# print(local_solar_time(datetime.datetime.now(),82.5,82.5))
-print(local_sidereal_time(datetime.datetime.now(),82.5,82.5))
